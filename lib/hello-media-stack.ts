02f7ce122f248ceddc649f33410c6f5cf5d854e1
Dade Sheng <dasheng@amazon.com>
2020-12-11 18:50:16 -0500

    add video job template

--- lib/hello-media-stack.ts
+++ lib/hello-media-stack.ts
@@ -1,5 +1,7 @@
+import * as mediaconvert from '@aws-cdk/aws-mediaconvert';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
+import { getJobTemplate } from '../config/job_template_settings';
 
 export class HelloMediaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,6 +9,12 @@ export class HelloMediaStack extends cdk.Stack {
 
     const bucket = new s3.Bucket(this, 'MediaBucket', {
       removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const template = getJobTemplate(bucket.bucketName);
+    const jobTemplate = new mediaconvert.CfnJobTemplate(this, 'JobTemplate', {
+      settingsJson: template.Settings,
+      name: template.Name
     });
   }
 }
