36ae19f25e57c87ae20765c8fa723adccbf95693
Dade Sheng <dasheng@amazon.com>
2020-12-11 18:03:47 -0500

    add a bucket

--- lib/hello-media-stack.ts
+++ lib/hello-media-stack.ts
@@ -1,9 +1,12 @@
+import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 
 export class HelloMediaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MediaBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
